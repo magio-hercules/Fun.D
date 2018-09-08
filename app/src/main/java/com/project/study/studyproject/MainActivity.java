bf9e500d1335f68ef2a679bbaf4a2706f77078cf
hercules <kjmhercules@gmail.com>
2018-09-08 22:14:42 +0900

    Initial commit

---
+++ app/src/main/java/com/project/study/studyproject/MainActivity.java
@@ -0,0 +1,69 @@
+package com.project.study.studyproject;
+
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity
+                            implements Memo1Fragment.OnFragmentInteractionListener,
+                                         Memo2Fragment.OnFragmentInteractionListener,
+                                         GalleryFragment.OnFragmentInteractionListener,
+                                         MapFragment.OnFragmentInteractionListener {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Adding Toolbar to the activity
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        // Initializing the TabLayout
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Memo 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Memo 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Gallery"));
+        tabLayout.addTab(tabLayout.newTab().setText("Map"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        // Initializing ViewPager
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+        // Creating TabPagerAdapter adapter
+        TabPagerAdapter pagerAdapter = new TabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        // Set TabSelectedListener
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri){
+        //you can leave it empty
+    }
+}
---
+++ app/src/main/java/com/project/study/studyproject/TabPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.project.study.studyproject;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class TabPagerAdapter extends FragmentStatePagerAdapter {
+
+    // Count number of tabs
+    private int tabCount;
+
+    public TabPagerAdapter(FragmentManager fm, int tabCount) {
+        super(fm);
+        this.tabCount = tabCount;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        // Returning the current tabs
+        switch (position) {
+            case 0:
+                Memo1Fragment memo1Fragment = new Memo1Fragment();
+                return memo1Fragment;
+            case 1:
+                Memo2Fragment memo2Fragment = new Memo2Fragment();
+                return memo2Fragment;
+            case 2:
+                GalleryFragment galleryFragment = new GalleryFragment();
+                return galleryFragment;
+            case 3:
+                MapFragment mapFragment = new MapFragment();
+                return mapFragment;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+
+}
