45be2989960da4b5e16f7c3c0e56511ea0181937
hercules <kjmhercules@gmail.com>
2018-11-27 14:36:09 +0900

    Splash 기능 추가

---
+++ 2.EasyCalendar/app/src/main/java/study/easycalendar/SplashActivity.java
@@ -0,0 +1,19 @@
+package study.easycalendar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("state", "launch");
+        startActivity(intent);
+        finish();
+    }
+}
