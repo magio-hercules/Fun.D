6463911c24a2f27a0fcab2256f3f408feb56fa3d
iamlangka <langka@nate.com>
2018-11-28 22:01:06 +0900

    dday 레이아웃 작업1
    dday 레이아웃 작업중

--- 2.EasyCalendar/app/src/main/java/study/easycalendar/DdayActivity.java
+++ 2.EasyCalendar/app/src/main/java/study/easycalendar/DdayActivity.java
@@ -1,9 +1,12 @@
 package study.easycalendar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DdayActivity extends AppCompatActivity {
 
@@ -18,5 +21,18 @@ public class DdayActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         // TODO
+
+        ImageView newDday = (ImageView) findViewById(R.id.iv_new_dday);
+
+        newDday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent;
+                intent = new Intent(getApplicationContext(), DdayEditActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
