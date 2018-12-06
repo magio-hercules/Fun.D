130ec2b64fcfe1fd0363d03fd5c3db448a6627d4
hercules <kjmhercules@gmail.com>
2018-12-06 23:22:09 +0900

    일정추가 기능 Layout 작성 완료

--- 2.EasyCalendar/app/src/main/java/study/easycalendar/DetailActivity.java
+++ 2.EasyCalendar/app/src/main/java/study/easycalendar/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,14 +14,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 import study.easycalendar.list.ListActivity;
 
 public class DetailActivity extends AppCompatActivity
             implements NavigationView.OnNavigationItemSelectedListener {
+    public static final String TAG = "Detail";
+
+    Spinner spinner_category;
+    Spinner spinner_repeat;
+    Spinner spinner_notification;
 
     DrawerLayout drawer;
+
+    // Calendar
+    Calendar calendar;
+    DatePicker picker_start_date;
+    DatePicker picker_end_date;
+    TimePicker picker_start_time;
+    TimePicker picker_end_time;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +69,9 @@ public class DetailActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        initSpinner();
+        initDateTime();
     }
 
     @Override
@@ -102,5 +128,67 @@ public class DetailActivity extends AppCompatActivity
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void initSpinner() {
+        spinner_category = (Spinner)findViewById(R.id.spinner_category);
+        spinner_repeat = (Spinner)findViewById(R.id.spinner_repeat);
+        spinner_notification = (Spinner)findViewById(R.id.spinner_notification);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.category, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_category.setAdapter(adapter);
+        spinner_category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(),
+                        parent.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        adapter = ArrayAdapter.createFromResource(
+                this, R.array.repeat, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_repeat.setAdapter(adapter);
+        spinner_repeat.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(),
+                               parent.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        adapter = ArrayAdapter.createFromResource(
+                this, R.array.notification, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_notification.setAdapter(adapter);
+        spinner_notification.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(),
+                        parent.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+    }
+
+    private void initDateTime() {
+        picker_start_date = (DatePicker) findViewById(R.id.picker_start_date);
+        picker_end_date = (DatePicker) findViewById(R.id.picker_end_date);
+        picker_start_time = (TimePicker) findViewById(R.id.picker_start_time);
+        picker_end_time = (TimePicker) findViewById(R.id.picker_end_time);
+
+        calendar = Calendar.getInstance();
+        int hour = calendar.get(calendar.HOUR_OF_DAY);
+        int min = calendar.get(calendar.MINUTE);
+        int sec = calendar.get(calendar.SECOND);
+
+        Log.d(TAG, "현재 시간 (" + hour + "시 " + min + "분 " + sec + "초)");
     }
 }
