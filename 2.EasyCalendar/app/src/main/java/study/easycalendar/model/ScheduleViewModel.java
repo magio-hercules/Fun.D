28e2a014fa656295358ae28f5cdf6f54cb0939c0
slee8789 <slee8789@naver.com>
2018-12-19 21:59:31 +0900

    - 상세페이지로 이동 추가

--- 2.EasyCalendar/app/src/main/java/study/easycalendar/model/ScheduleViewModel.java
+++ 2.EasyCalendar/app/src/main/java/study/easycalendar/model/ScheduleViewModel.java
@@ -3,6 +3,8 @@ package study.easycalendar.model;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.util.Log;
@@ -22,6 +24,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import study.easycalendar.DetailActivity;
 import study.easycalendar.model.local.AppDatabase;
 import study.easycalendar.model.local.ScheduleDao;
 
@@ -30,11 +33,13 @@ public class ScheduleViewModel extends AndroidViewModel {
     private Schedule schedule;
     private ScheduleDao scheduleDao;
     private ExecutorService executorService;
+    private Context context;
 
     public ScheduleViewModel(@NonNull Application application) {
         super(application);
         scheduleDao = AppDatabase.getInstance(application).scheduleDao();
         executorService = Executors.newSingleThreadExecutor();
+        context = application;
     }
 
     public void setSchedule(Schedule schedule) {
@@ -43,7 +48,10 @@ public class ScheduleViewModel extends AndroidViewModel {
 
 
     public void onItemClick() {
-        executorService.execute(() -> scheduleDao.deleteSchedule(schedule));
+//        executorService.execute(() -> scheduleDao.deleteSchedule(schedule));
+        Intent intent = new Intent(context, DetailActivity.class);
+        intent.putExtra("id", schedule.id);
+        context.startActivity(intent);
     }
 
     public boolean onItemLongClick() {
