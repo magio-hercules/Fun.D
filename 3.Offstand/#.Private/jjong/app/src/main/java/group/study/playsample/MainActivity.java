f9cc6ee227a4c253a80f89c742d7369d9ab57b0a
hercules <kjmhercules@gmail.com>
2019-03-13 01:03:05 +0900

    테스트 앱 추가

---
+++ 3.Offstand/#.Private/jjong/app/src/main/java/group/study/playsample/MainActivity.java
@@ -0,0 +1,317 @@
+package group.study.playsample;
+
+import android.content.Intent;
+import android.support.animation.DynamicAnimation;
+import android.support.animation.SpringAnimation;
+import android.support.animation.SpringForce;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.FrameLayout.LayoutParams;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image1;
+    ImageView image2;
+    ImageView image3;
+    ImageView image4;
+
+    TextView text1;
+    TextView text2;
+
+    Button button_reset;
+    Button button_shuffle;
+
+    int windowwidth;
+    int windowheight;
+
+    int viewHeight;
+
+    private LayoutParams layoutParams;
+    int startY;
+    int startMarginTop;
+
+    private SpringAnimation xAnimation;
+    private SpringAnimation yAnimation;
+    private SpringAnimation openAnimation;
+
+    private SpringAnimation ani_view1_x, ani_view1_y;
+    private SpringAnimation ret_view1_x, ret_view1_y;
+    private SpringAnimation ani_view2_x, ani_view2_y;
+    private SpringAnimation ret_view2_x, ret_view2_y;
+
+    private float org_x;
+    private float org_y;
+
+    private float dX;
+    private float dY;
+
+    boolean bCheck = false;
+    int tempIndex = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        windowwidth = getWindowManager().getDefaultDisplay().getWidth();
+        windowheight = getWindowManager().getDefaultDisplay().getHeight();
+        image1 = (ImageView) findViewById(R.id.ImageView01);
+        image2 = (ImageView) findViewById(R.id.ImageView02);
+        image3 = (ImageView) findViewById(R.id.ImageView03);
+        image4 = (ImageView) findViewById(R.id.ImageView04);
+
+
+        text1 = (TextView) findViewById(R.id.text_card1);
+        text2 = (TextView) findViewById(R.id.text_card2);
+
+        button_reset = (Button) findViewById(R.id.button_reset);
+        button_reset.setEnabled(false);
+
+        button_shuffle = (Button) findViewById(R.id.button_shuffle);
+
+        image1.getViewTreeObserver().addOnGlobalLayoutListener(globalLayoutListenerView1);
+        image2.getViewTreeObserver().addOnGlobalLayoutListener(globalLayoutListenerView2);
+
+        image2.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                LayoutParams layoutParams = (LayoutParams) image2.getLayoutParams();
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+//                        startY = (int) event.getRawY();
+//                        startMarginTop = layoutParams.topMargin;
+//                        Log.d("[PLAY]", "startY : " + startY);
+
+                        if (button_reset.isEnabled()) {
+                            return false;
+                        }
+
+                        viewHeight = v.getHeight();
+                        Log.d("[PLAY]", "v.getHeight() : " + viewHeight);
+                        Log.d("[PLAY]", "v.getY() : " + v.getY());
+                        Log.d("[PLAY]", "event.getRawY() : " + event.getRawY());
+
+                        startY = (int) event.getRawY();
+
+                        // capture the difference between view's top left corner and touch point
+//                        dX = v.getX() - event.getRawX();
+                        dY = v.getY() - event.getRawY();
+                        // cancel animations
+                        xAnimation.cancel();
+                        yAnimation.cancel();
+                        break;
+                    case MotionEvent.ACTION_UP:
+//                        startY = (int) event.getRawY();
+//                        startMarginTop = layoutParams.topMargin;
+//                        Log.d("[PLAY]", "startY : " + startY);
+
+
+                        int gap = startY > (int)event.getRawY() ? startY - (int)event.getRawY() : (int)event.getRawY() - startY;
+                        Log.d("[PLAY]", "viewHeight/2 : " + (viewHeight/2) + ", gap : " + gap);
+
+                        // 패가 절반이상 까진경우
+                        if (viewHeight/2 < gap) {
+                            Log.d("[PLAY]", "패가 절반이상 까진경우");
+//                            openAnimation.start();
+                            bCheck = true;
+                        }
+
+//                        xAnimation.start();
+                        yAnimation.start();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+//                        int x_cord = (int) event.getRawX();
+//                        int y_cord = (int) event.getRawY();
+//
+//                        Log.d("[PLAY]", "y_cord : " + y_cord);
+//
+//                        if (x_cord > windowwidth) {
+//                            x_cord = windowwidth;
+//                        }
+//                        if (y_cord > windowheight) {
+//                            y_cord = windowheight;
+//                        }
+//
+////                        layoutParams.leftMargin = x_cord - 25;
+////                        layoutParams.topMargin = y_cord - 75;
+////                        layoutParams.leftMargin = x_cord;
+////                        layoutParams.topMargin = y_cord;
+////                        image2.setLayoutParams(layoutParams);
+//
+//                        int gap = y_cord - startY;
+//                        Log.d("[PLAY]", "gap : " + gap);
+////                        if (startY < y_cord) {
+////                            gap = y_cord - startY;
+////                            Log.d("[PLAY]", "gap : " + gap);
+////                        } else {
+////                            gap = startY - y_cord;
+////                            Log.d("[PLAY]", "gap : " + gap);
+////                        }
+////                        layoutParams.topMargin = y_cord;
+//                        layoutParams.topMargin = startMarginTop + gap;
+//                        image2.setLayoutParams(layoutParams);
+
+
+                        Log.d("[PLAY]", "v.getY() : " + v.getY() + ", event.getRawY() : " + event.getRawY());
+
+                        //  a different approach would be to change the view's LayoutParams.
+                        image2.animate()
+//                                .x(event.getRawX() + dX)
+                                .y(event.getRawY() + dY)
+                                .setDuration(0)
+                                .start();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
+        button_reset.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                resetCard();
+            }
+        });
+
+        button_shuffle.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShuffleActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public static SpringAnimation createSpringAnimation(View view,
+                                                        DynamicAnimation.ViewProperty property,
+                                                        float finalPosition,
+                                                        float stiffness,
+                                                        float dampingRatio) {
+        SpringAnimation animation = new SpringAnimation(view, property);
+        SpringForce springForce = new  SpringForce(finalPosition);
+        springForce.setStiffness(stiffness);
+        springForce.setDampingRatio(dampingRatio);
+        animation.setSpring(springForce);
+        return animation;
+    }
+
+    // create X and Y animations for view's initial position once it's known
+    private ViewTreeObserver.OnGlobalLayoutListener globalLayoutListenerView1 = new ViewTreeObserver.OnGlobalLayoutListener() {
+        @Override
+        public void onGlobalLayout() {
+            ani_view1_x = createSpringAnimation(image1, SpringAnimation.X, image4.getX(),
+                    SpringForce.STIFFNESS_LOW, SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+            ani_view1_y = createSpringAnimation(image1, SpringAnimation.Y, image4.getY(),
+                    SpringForce.STIFFNESS_LOW, SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+
+            org_x = image1.getX();
+            org_y = image1.getY();
+
+            ret_view1_x = createSpringAnimation(image1, SpringAnimation.X, org_x,
+                    SpringForce.STIFFNESS_LOW, SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+            ret_view1_y = createSpringAnimation(image1, SpringAnimation.Y, org_y,
+                    SpringForce.STIFFNESS_LOW, SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+
+            ret_view1_x.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+                @Override
+                public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                    Log.d("[PLAY]", "Reset 완료");
+                    image1.setImageResource(R.drawable.card_3_1);
+                    image2.setImageResource(R.drawable.card_back);
+                }
+            });
+        }
+    };
+
+    // create X and Y animations for view's initial position once it's known
+    private ViewTreeObserver.OnGlobalLayoutListener globalLayoutListenerView2 = new ViewTreeObserver.OnGlobalLayoutListener() {
+        @Override
+        public void onGlobalLayout() {
+            xAnimation = createSpringAnimation(image2, SpringAnimation.X, image1.getX(),
+                    SpringForce.STIFFNESS_LOW, SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+            yAnimation = createSpringAnimation(image2, SpringAnimation.Y, image1.getY(),
+                    SpringForce.STIFFNESS_LOW, SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+//            openAnimation = createSpringAnimation(image2, SpringAnimation.Y, image1.getY() + image1.getHeight(),
+//                    SpringForce.STIFFNESS_LOW, SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+
+            ani_view2_x = createSpringAnimation(image2, SpringAnimation.X, image3.getX(),
+                    SpringForce.STIFFNESS_LOW, SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+            ani_view2_y = createSpringAnimation(image2, SpringAnimation.Y, image3.getY(),
+                    SpringForce.STIFFNESS_LOW, SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+
+            org_x = image2.getX();
+            org_y = image2.getY();
+
+            ret_view2_x = createSpringAnimation(image2, SpringAnimation.X, org_x,
+                    SpringForce.STIFFNESS_LOW, SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+            ret_view2_y = createSpringAnimation(image2, SpringAnimation.Y, org_y,
+                    SpringForce.STIFFNESS_LOW, SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+
+            yAnimation.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+                @Override
+                public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                    if (bCheck) {
+                        changeCard();
+                    }
+                }
+            });
+        }
+    };
+
+
+
+    private void changeCard() {
+        if (tempIndex == 1) {
+            text1.setText("첫번째 패 : 3光");
+            Toast.makeText(this, "3光", Toast.LENGTH_SHORT).show();
+            image1.setImageResource(R.drawable.card_8_1);
+            image2.setImageResource(R.drawable.card_3_1);
+            tempIndex++;
+            bCheck = false;
+        } else  if (tempIndex == 2){
+            text2.setText("두번째 패 : 8光");
+            Toast.makeText(this, "8光", Toast.LENGTH_SHORT).show();
+//            image1.setImageResource(R.drawable.card_8_1);
+//            image2.setImageResource(R.drawable.card_3_1);
+//            image3.setImageResource(R.drawable.card_8_1);
+//            image4.setImageResource(R.drawable.card_3_1);
+            tempIndex = 1;
+            bCheck = false;
+
+            button_reset.setEnabled(true);
+            // move
+            ani_view2_x.start();
+            ani_view2_y.start();
+            ani_view1_x.start();
+            ani_view1_y.start();
+        }
+    }
+
+    private void resetCard() {
+        tempIndex = 1;
+        bCheck = false;
+        image1.setImageResource(R.drawable.card_back);
+        image2.setImageResource(R.drawable.card_back);
+        image3.setImageResource(0);
+        image4.setImageResource(0);
+        text1.setText("첫번째 패 : ");
+        text2.setText("두번째 패 : ");
+
+        button_reset.setEnabled(false);
+
+        ret_view2_x.start();
+        ret_view2_y.start();
+        ret_view1_x.start();
+        ret_view1_y.start();
+    }
+}
---
+++ 3.Offstand/#.Private/jjong/app/src/main/java/group/study/playsample/ShuffleActivity.java
@@ -0,0 +1,246 @@
+package group.study.playsample;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.animation.DynamicAnimation;
+import android.support.animation.SpringAnimation;
+import android.support.animation.SpringForce;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.FrameLayout.LayoutParams;
+import android.widget.Toast;
+
+public class ShuffleActivity extends Activity {
+    Button button_main;
+    Button button_shuffle;
+    Button button_test;
+
+    ImageView shuffleCard1, shuffleCard2, shuffleCard3;
+    ImageView card1, card2, card3, card4;
+
+    private SpringAnimation shuffleAniX1, shuffleAniY1;
+    private SpringAnimation shuffleAniX2, shuffleAniY2;
+    private SpringAnimation shuffleAniX3, shuffleAniY3;
+
+    int shuffleCount = 0;
+    boolean bShuffle = false;
+
+    public ShuffleActivity() {
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shuffle);
+
+
+        button_main = (Button) findViewById(R.id.button_main);
+        button_main.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ShuffleActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button_shuffle = (Button) findViewById(R.id.button_shuffle);
+        button_shuffle.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (!bShuffle) {
+                    bShuffle = true;
+                    shuffle(1);
+                } else {
+                    bShuffle = false;
+                }
+            }
+        });
+
+        button_test = (Button) findViewById(R.id.button_test);
+        button_test.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                test();
+            }
+        });
+
+
+        shuffleCard1 = (ImageView) findViewById(R.id.cardShuffle1);
+        shuffleCard2 = (ImageView) findViewById(R.id.cardShuffle2);
+        shuffleCard3 = (ImageView) findViewById(R.id.cardShuffle3);
+//        shuffleCard1.getViewTreeObserver().addOnGlobalLayoutListener(globalLayoutListenerShuffleCard1);
+//        shuffleCard2.getViewTreeObserver().addOnGlobalLayoutListener(globalLayoutListenerShuffleCard2);
+//        shuffleCard3.getViewTreeObserver().addOnGlobalLayoutListener(globalLayoutListenerShuffleCard3);
+
+        card1 = (ImageView) findViewById(R.id.card1);
+        card2 = (ImageView) findViewById(R.id.card2);
+        card3 = (ImageView) findViewById(R.id.card3);
+        card4 = (ImageView) findViewById(R.id.card4);
+
+    }
+
+    private void shuffle(int index) {
+        Log.d("[Shuffle]", "shuffle");
+
+        switch (index) {
+            case 1:
+                translateAni(shuffleCard1, card2, index);
+                break;
+            case 2:
+                translateAni(shuffleCard2, card3, index);
+                break;
+            case 3:
+                translateAni(shuffleCard3, card4, index);
+                break;
+        }
+    }
+
+
+    public static SpringAnimation createSpringAnimation(View view,
+                                                        DynamicAnimation.ViewProperty property,
+                                                        float finalPosition,
+                                                        float stiffness,
+                                                        float dampingRatio) {
+        SpringAnimation animation = new SpringAnimation(view, property);
+        SpringForce springForce = new  SpringForce(finalPosition);
+        springForce.setStiffness(stiffness);
+        springForce.setDampingRatio(dampingRatio);
+        animation.setSpring(springForce);
+        return animation;
+    }
+
+    private void shuffleAnimation(ImageView srcImage, ImageView dstImage) {
+        Log.d("[Shuffle]", "shuffleAnimation");
+
+        shuffleAniX1 = createSpringAnimation(srcImage, SpringAnimation.X, dstImage.getX(),
+                SpringForce.STIFFNESS_LOW, SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+        shuffleAniY1 = createSpringAnimation(srcImage, SpringAnimation.Y, dstImage.getY(),
+                SpringForce.STIFFNESS_LOW, SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+    }
+
+    private void translateAni(ImageView src, ImageView dst, final int index) {
+        int gapX = 30, gapY = 30;
+
+        TranslateAnimation animation = new TranslateAnimation(
+                0,
+                dst.getX()-src.getX()+(gapX*shuffleCount),
+                0 ,
+                dst.getY()-src.getY()+(gapY*shuffleCount));
+        animation.setRepeatMode(0);
+        animation.setDuration(350);
+        animation.setFillAfter(true);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                if (!bShuffle) {
+                    return;
+                }
+
+                switch (index) {
+                    case 1:
+                        card2.setVisibility(View.VISIBLE);
+                        shuffle(2);
+                        break;
+                    case 2:
+                        card3.setVisibility(View.VISIBLE);
+                        shuffle(3);
+                        break;
+                    case 3:
+                        if (shuffleCount > 0) {
+                            shuffleCount = 0;
+                            return;
+                        }
+                        card4.setVisibility(View.VISIBLE);
+                        shuffleCount++;
+                        shuffle(1);
+                        break;
+                }
+            }
+            @Override
+            public void onAnimationStart(Animation animation) { }
+            @Override
+            public void onAnimationRepeat(Animation animation) { }
+        });
+
+        src.startAnimation(animation);
+    }
+
+//    private ViewTreeObserver.OnGlobalLayoutListener globalLayoutListenerShuffleCard1 = new ViewTreeObserver.OnGlobalLayoutListener() {
+//        @Override
+//        public void onGlobalLayout() {
+//            shuffleAniX1.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+//                @Override
+//                public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+////                    shuffleAniX1.cancel();
+////                    shuffleAniY1.cancel();
+//
+////                    shuffleAnimation(shuffleCard, card3);
+//                    shuffle(2);
+//                }
+//            });
+//        }
+//    };
+//
+//    private ViewTreeObserver.OnGlobalLayoutListener globalLayoutListenerShuffleCard2 = new ViewTreeObserver.OnGlobalLayoutListener() {
+//        @Override
+//        public void onGlobalLayout() {
+//            shuffleAniX1.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+//                @Override
+//                public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+////                    shuffleAnimation(shuffleCard, card3);
+//                    shuffle(3);
+//                }
+//            });
+//        }
+//    };
+//
+//    private ViewTreeObserver.OnGlobalLayoutListener globalLayoutListenerShuffleCard3 = new ViewTreeObserver.OnGlobalLayoutListener() {
+//        @Override
+//        public void onGlobalLayout() {
+//            shuffleAniX1.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+//                @Override
+//                public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+////                    shuffleAnimation(shuffleCard, card3);
+//                    shuffle(1);
+//                }
+//            });
+//        }
+//    };
+
+
+
+        private void test() {
+    }
+
+    private void resetShuffleCard() {
+//        Log.d("[Shuffle]", "resetShuffleCard");
+//        LayoutParams layoutParams = (LayoutParams) shuffleCard.getLayoutParams();
+//        Log.d("[Shuffle]", "resetShuffleCard layoutParams : (" + layoutParams.leftMargin + ", " + layoutParams.topMargin + ", " + layoutParams.rightMargin + ", " + layoutParams.bottomMargin + ")");
+//
+//        LayoutParams orgPos = (LayoutParams) card1.getLayoutParams();
+////
+//        LayoutParams params = new LayoutParams(card1.getWidth(), card1.getHeight());
+//        params.setMargins(orgPos.leftMargin, orgPos.topMargin, orgPos.rightMargin, orgPos.bottomMargin);
+//
+//        shuffleCard.setLayoutParams(params);
+//
+////        ViewGroup.MarginLayoutParams mp = new ViewGroup.MarginLayoutParams(card1.getLayoutParams());
+////        mp.setMargins(orgPos.leftMargin, orgPos.topMargin, orgPos.rightMargin, orgPos.bottomMargin);
+////        FrameLayout.LayoutParams fp = new LayoutParams(mp);
+////        shuffleCard.setLayoutParams(fp);
+//
+//        layoutParams = (LayoutParams) shuffleCard.getLayoutParams();
+//        Log.d("[Shuffle]", "resetShuffleCard layoutParams : (" + layoutParams.leftMargin + ", " + layoutParams.topMargin + ", " + layoutParams.rightMargin + ", " + layoutParams.bottomMargin + ")");
+    }
+
+}
