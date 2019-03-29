84e83f7b36ab86d1d18b2ea3765ce4ec26a2fa5d
feelsogood <48210879+feelsogoodtime@users.noreply.github.com>
2019-03-29 20:34:06 +0900

    퍼미션 체크 -> 위치 권환이 ACCESS_FINE인지 확실친 않아요;;

--- 3.Offstand/#.Private/leesc/app/src/main/java/com/leesc/tazza/ui/splash/SplashActivity.java
+++ 3.Offstand/#.Private/leesc/app/src/main/java/com/leesc/tazza/ui/splash/SplashActivity.java
@@ -38,6 +38,14 @@ public class SplashActivity extends BaseActivity<ActivitySplashBinding, SplashVi
         super.onCreate(savedInstanceState);
         splashViewModel.setNavigator(this);
         Log.d("lsc","SplashActivity onCreate");
+		
+		//마시멜로우 이상 버전 권한 체크
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+	
     }
 
     @Override
@@ -57,4 +65,25 @@ public class SplashActivity extends BaseActivity<ActivitySplashBinding, SplashVi
     public void handleError(Throwable throwable) {
 
     }
+
+	//마시멜로우 이상 버전 권한 체크
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "위치 권한 승인", Toast.LENGTH_LONG).show();
+                    } else if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(this, "위치 권한 거부.", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(this, "위치 권한 승인 부여 받지 못함", Toast.LENGTH_LONG).show();
+                }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+	 	
+		
+		
 }
