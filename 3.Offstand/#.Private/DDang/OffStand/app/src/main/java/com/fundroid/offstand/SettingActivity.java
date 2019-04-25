bb051d1b93bc781cce66b2766850e1f97ffaa224
unique3552 <wor0@naver.com>
2019-04-25 23:40:28 +0900

    개인통계 밑바탕만!
    이것만이라도 했어용ㅎㅎ

--- 3.Offstand/#.Private/DDang/OffStand/app/src/main/java/com/fundroid/offstand/SettingActivity.java
+++ 3.Offstand/#.Private/DDang/OffStand/app/src/main/java/com/fundroid/offstand/SettingActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.fundroid.offstand.data.remote.ConnectionManager;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnTouch;
 import io.reactivex.schedulers.Schedulers;
 
 import static com.fundroid.offstand.core.AppConstant.ROOM_PORT;
@@ -32,7 +36,38 @@ public class SettingActivity extends AppCompatActivity {
 
         // 버터나이프 사용
         ButterKnife.bind(this);
+
+        FrameLayout state_black = (FrameLayout) findViewById(R.id.fragment_container_stats);
+        state_black.setVisibility(View.GONE);
+
+        ImageView stats_save = (ImageView) findViewById(R.id.setting_stats_save);
+        stats_save.setVisibility(View.GONE);
     }
+
+    @OnClick(R.id.setting_stats)
+    public void Stats_Click(){
+        FrameLayout state_black = (FrameLayout) findViewById(R.id.fragment_container_stats);
+        state_black.setVisibility(View.VISIBLE);
+
+        ImageView stats_save = (ImageView) findViewById(R.id.setting_stats_save);
+        stats_save.setVisibility(View.VISIBLE);
+    }
+
+    @OnClick(R.id.setting_stats_save)
+    public void Save_Click(){
+        FrameLayout state_black = (FrameLayout) findViewById(R.id.fragment_container_stats);
+        state_black.setVisibility(View.GONE);
+
+        ImageView stats_save = (ImageView) findViewById(R.id.setting_stats_save);
+        stats_save.setVisibility(View.GONE);
+    }
+
+    // 해당 id 뒤에 있는 객체에 대해서 이벤트 처리를 어떻게 할 것인가를 정하는 기능([return] true : 불가능 / false : 가능)
+    @OnTouch(R.id.fragment_container_stats)
+    public boolean Click_block(){
+        return true;
+    }
+
 
 //    @OnClick(R.id.button_save)
 //    public void Save() {
