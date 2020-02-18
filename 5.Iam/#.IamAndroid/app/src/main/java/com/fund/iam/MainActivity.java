7b9400971f05406523ac99c8bce8bd378ea8d4e0
쫑미니 <kjmhercules@gmail.com>
2020-02-18 18:20:20 +0900

    프로젝트 껍데기 생성

--- 5.Iam/#.IamAndroid/app/src/main/java/com/fund/iam/MainActivity.java
+++ 5.Iam/#.IamAndroid/app/src/main/java/com/fund/iam/MainActivity.java
@@ -1,34 +1,45 @@
 package com.fund.iam;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.fund.iam.fragment.TestFragment;
+import com.fund.iam.fragment.HomeFragment;
+
+public class MainActivity extends AppCompatActivity
+        implements TestFragment.OnFragmentInteractionListener,
+                     HomeFragment.OnFragmentInteractionListener {
+    private static final String TAG = "[IAM][MAIN]";
+
+    BottomNavigationView bottomNavigation;
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        initBottomNavigation();
     }
 
     @Override
@@ -51,5 +62,73 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    private void initBottomNavigation() {
+        Log.d(TAG, "initBottomNavigation");
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        navigationItemSelectedListener =
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.navigation_test:
+                                Log.d(TAG, "navigation_test");
+                                openFragment(TestFragment.newInstance("", ""));
+                                return true;
+                            case R.id.navigation_home:
+                                Log.d(TAG, "navigation_home fragment");
+                                openFragment(HomeFragment.newInstance("", ""));
+                                return true;
+                            case R.id.navigation_bookmark:
+                                Log.d(TAG, "navigation_bookmark");
+//                                openFragment(SmsFragment.newInstance("", ""));
+                                return true;
+                            case R.id.navigation_search:
+                                Log.d(TAG, "navigation_search");
+//                                openFragment(NotificationFragment.newInstance("", ""));
+                                return true;
+                            case R.id.navigation_setting:
+                                Log.d(TAG, "navigation_setting");
+//                                openFragment(NotificationFragment.newInstance("", ""));
+                                return true;
+                        }
+                        return false;
+                    }
+                };
+
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(TestFragment.newInstance("", ""));
+    }
+
+    @Override
+    public void onFragmentInteraction(String fragmentName){
+        Log.d(TAG, "MainActivity onFragmentInteraction()");
+
+        switch (fragmentName) {
+            case "test":
+                openFragment(TestFragment.newInstance("", ""));
+                break;
+            case "login":
+//                openFragment(TestFragment.newInstance("", ""));
+                break;
+            case "home":
+                openFragment(HomeFragment.newInstance("", ""));
+                break;
+            case "boomark":
+                break;
+            case "search":
+                break;
+            case "setting":
+                break;
+        }
     }
 }
