7829623f113183bf015abad7a2cb3db4e5c27976
raonshift <raonshift@gmail.com>
2021-01-11 23:56:49 +0900

    [Home Page] 운영진소개 페이지 업데이트
    [Home Page] 운영진소개 페이지 업데이트

--- 0.Homepage/public/js/team.js
+++ 0.Homepage/public/js/team.js
@@ -1,7 +1,66 @@
-$(function() {
-    $("dt").click(function() {
-        $(this).toggleClass("open");
-        if ($(this).hasClass("open"))
-            $("dt").not(this).removeClass("open");
-    })
-})
+  /*--------------------------
+              Managerment introduce 
+              ---------------------------- */
+  $(function() {
+          $("dt").click(function() {
+              $(this).toggleClass("open");
+              if ($(this).hasClass("open"))
+                  $("dt").not(this).removeClass("open");
+          })
+      })
+      /*---------------------
+   TOP Menu Stick
+  --------------------- */
+  var s = $("#sticker");
+  var pos = s.position();
+  $(window).on("scroll", function() {
+      var windowpos = $(window).scrollTop() > 300;
+      if (windowpos > pos.top) {
+          s.addClass("stick");
+      } else {
+          s.removeClass("stick");
+      }
+  });
+
+  /*----------------------------
+     Navbar nav
+    ------------------------------ */
+  var main_menu = $(".main-menu ul.navbar-nav li ");
+  main_menu.on("click", function() {
+      main_menu.removeClass("active");
+      $(this).addClass("active");
+  });
+  /*----------------------------
+     wow js active
+    ------------------------------ */
+  new WOW().init();
+
+  $(".navbar-collapse a:not(.dropdown-toggle)").on("click", function() {
+      $(".navbar-collapse.collapse").removeClass("in");
+  });
+
+  /*----------------------------
+     Scrollspy js
+    ------------------------------ */
+  var Body = $("body");
+  Body.scrollspy({
+      target: ".navbar-collapse",
+      offset: 80,
+  });
+
+  /*----------------------------
+    Page Scroll
+    ------------------------------ */
+  var page_scroll = $("a.page-scroll");
+  page_scroll.on("click", function(event) {
+      var $anchor = $(this);
+      $("html, body")
+          .stop()
+          .animate({
+                  scrollTop: $($anchor.attr("href")).offset().top - 55,
+              },
+              1500,
+              "easeInOutExpo"
+          );
+      event.preventDefault();
+  });
